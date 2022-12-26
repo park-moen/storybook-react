34e6d62049f859cee9ee60276caf80ac94dd0570
parkmoen <pmj6516@naver.com>
2022-12-26 14:41:53 +0900

    add storybook Text component

---
+++ src/stories/Text.stories.js
@@ -0,0 +1,40 @@
+import Text from '../components/Text';
+
+export default {
+  name: 'Component/Text',
+  component: Text,
+  argTypes: {
+    size: { control: 'number' },
+    strong: { control: 'boolean' },
+    underline: { control: 'boolean' },
+    delete: { control: 'boolean' },
+    color: { control: 'color' },
+    block: { control: 'boolean' },
+    paragraph: { control: 'boolean' },
+    mark: { control: 'boolean' },
+    code: { control: 'boolean' },
+  },
+};
+
+export const Default = args => {
+  return <Text {...args}>Text</Text>;
+};
+
+export const Size = args => {
+  return (
+    <>
+      <Text size='large' {...args}>
+        large
+      </Text>
+      <Text size='normal' {...args}>
+        normal
+      </Text>
+      <Text size='small' {...args}>
+        small
+      </Text>
+      <Text size={24} {...args}>
+        CUstom
+      </Text>
+    </>
+  );
+};
